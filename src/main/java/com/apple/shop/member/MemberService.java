752c6a26a690706127434943182120d418a4e360
Wjs_Spring <woojs37@naver.com>
2025-08-26 18:14:46 +0900

    MemberService.java 커밋 테스트

---
+++ src/main/java/com/apple/shop/member/MemberService.java
@@ -0,0 +1,48 @@
+package com.apple.shop.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    // 검증 가입 처리
+    @Transactional
+    public String signup(String username, String password, String passwordConfirm, String email) {
+        // 비밀번호 확인
+        if (!password.equals(passwordConfirm)) {
+            return "비밀번호가 일치하지 않습니다!";
+        }
+        // 아이디 중복 체크
+        if (memberRepository.findByUsername(username).isPresent()) {
+            return "이미 가입된 아이디입니다!";
+        }
+        // 정상 가입
+        Member member = new Member();
+        member.setUsername(username);
+        member.setPassword(passwordEncoder.encode(password));
+        member.setEmail(email);
+        member.setRole("USER");
+        memberRepository.save(member);
+        return null; // 에러 없음!
+    }
+    public MemberDTO getMyPageDto(String username) {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("회원 정보 없음"));
+        return new MemberDTO(
+                member.getUsername(),
+                member.getEmail(),
+                member.getCreatedAt(),
+                member.getRole()
+        );
+    }
+
+}
